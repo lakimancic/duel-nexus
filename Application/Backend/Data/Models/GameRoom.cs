ff7af3d91508a6e8de7b14d5a745e6eb0e315d11
lakimancic <lazarskolski@gmail.com>
2026-01-28 11:35:10 +0100

    feat(data): added game room entities

---
+++ Application/Backend/Data/Models/GameRoom.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Backend.Data.Enums;
+
+namespace Backend.Data.Models;
+
+public class GameRoom
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public bool IsRanked { get; set; }
+    public RoomStatus Status { get; set; }
+
+    public string? JoinCode { get; set; }
+
+    public Guid HostUserId { get; set; }
+    [ForeignKey(nameof(HostUserId))]
+    public User HostUser { get; set; } = null!;
+
+    public DateTime CreatedAtUtc { get; set; }
+
+    public ICollection<GameRoomPlayer> Players { get; set; } = [];
+}
