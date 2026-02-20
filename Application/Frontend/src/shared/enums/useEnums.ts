c951a3171e62d64bbc59c6c2e5b00878448c68c5
lakimancic <lazarskolski@gmail.com>
2026-02-20 10:49:52 +0100

    feat(front): card board fixed a little

--- Application/Frontend/src/shared/enums/useEnums.ts
+++ Application/Frontend/src/shared/enums/useEnums.ts
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
 import { useCardTypesStore } from "./cardTypes.store";
 import { useEffectTypesStore } from "./effectTypes.store";
+import { useCardZonesStore } from "./cardZones.store";
 
 export function useAdminEnums() {
   const loadCardTypes = useCardTypesStore(s => s.load);
   const loadEffectTypes = useEffectTypesStore(s => s.load);
+  const loadCardZones = useCardZonesStore(s => s.load);
 
   useEffect(() => {
     loadCardTypes();
     loadEffectTypes();
+    loadCardZones();
   }, [
     loadCardTypes,
     loadEffectTypes,
+    loadCardZones,
   ]);
 }
